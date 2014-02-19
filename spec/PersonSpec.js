45a9a8706a1ecf413047020b36c3f7da0092a2b2
K Mak <karen.mak@stx.oxon.org>
2014-02-19 17:23:35 -0500

    Completed all but the greetPeople method

--- spec/PersonSpec.js
+++ spec/PersonSpec.js
@@ -1,4 +1,100 @@
 describe('Person', function(){
+  var person;
+
+  beforeEach(function() {
+    person2 = new Person('John', 'Dover', args = {age: 37});
+    person3 = new Person('Mark', 'Dover', args = {weight: 50, height: 178});
+    person = new Person('Bill', 'Dover', args = {friends: [person2, person3]});
+  });
+
+  // it('should require a first and last name', function() {
+  //   // expect(person.first_name).not.toBeDefined();
+  //   // expect(person.last_name).not.toBeDefined();
+  //   expect(person).
+  // });
+
+  it('should have optional age, default to a random value between 25 and 45', function() {
+    expect(person.age).toBeLessThan(46);
+    expect(person.age).toBeGreaterThan(24);
+    expect(person2.age).toBe(37);
+  });
+
+  it('should have optional weight, default to a random value between 60 and 100', function() {
+    expect(person3.weight).toBe(50);
+    expect(person.weight).toBeLessThan(101);
+    expect(person.weight).toBeGreaterThan(59);
+  });
+
+  it('should have optional height, default to a random value between 150 and 200', function() {
+    expect(person3.height).toBe(178);
+    expect(person.height).toBeLessThan(201);
+    expect(person.height).toBeGreaterThan(149);
+  });
+
+  it('should have optional friends array, default to no friends', function () {
+    expect(person.friends).toEqual([person2, person3]);
+    expect(person2.friends).toEqual([]);
+  });
+
+  describe('Getting a persons full name', function() {
+    beforeEach(function() {
+      person.fullName();
+      person2.fullName();
+      person3.fullName();
+    });
+
+    it('Should return the persons full name', function(){
+      expect(person.full_name).toBe('Bill Dover');
+      expect(person2.full_name).toBe('John Dover');
+      expect(person3.full_name).toBe('Mark Dover');
+    });
+  });
+
+  describe('Getting converting the weight of a person in stone', function() {
+    beforeEach(function () {
+      person3.toStone();
+    });
+
+    it('Should return the weight in stones', function() {
+      expect(person3.weight_in_stones).toBe('7.873');
+
+    });
+  });
+
+  describe('Add to a persons friends list', function() {
+    beforeEach(function () {
+      person3.addFriend(person);
+      person.addFriend(person2);
+    });
+
+    it('Should add person to person3 friend list', function() {
+      expect(person3.friends).toEqual([person]);
+      expect(person.friends).toEqual([person2, person3, person2]);
+    });
+  });
 
 
+  describe('Delete a persons from friends list', function() {
+    beforeEach(function () {
+      person.deleteFriend(person2);
+    });
+
+    it('Should delete person to person3 friend list', function() {
+      expect(person.friends).toEqual([person3]);
+    });
+  });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Person.js
+++ src/Person.js
@@ -1,3 +1,28 @@
-var Person = function() {
+function Person(first_name, last_name, args){
+  args = args || {};
+  this.first_name = first_name;
+  this.last_name = last_name;
+  this.age = args.age || Math.floor(Math.random() * (45 - 25 + 1) + 25);
+  this.weight = args.weight || Math.floor(Math.random() * (100 - 60 + 1) + 60);
+  this.height = args.height || Math.floor(Math.random() * (200 - 150 + 1) + 150);
+  this.friends = args.friends || [];
+};
 
+
+Person.prototype.fullName = function() {
+  this.full_name = (this.first_name + ' ' + this.last_name);
+};
+
+Person.prototype.toStone = function() {
+  stones = this.weight * 0.15747;
+  this.weight_in_stones = stones.toFixed(3);
+};
+
+Person.prototype.addFriend = function(person) {
+  this.friends.push(person);
+};
+
+Person.prototype.deleteFriend = function(person) {
+  index = this.friends.indexOf(person);
+  this.friends.splice(index, 1);
 };
